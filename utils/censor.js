6db4b3108614601936aeadb7c9efc5c9b89f71e5
Alexandre7888 <braatzjunioralexandre5@gmail.com>
2026-08-13 23:38:24 -0300

    Delete utils directory

--- utils/censor.js
+++
@@ -1,7 +0,0 @@
-window.CensorUtils = {
-    censor: (text) => {
-        if (!text || typeof text !== 'string') return text;
-        // Substitui qualquer palavra que comece com bot_ (case insensitive) por ***
-        return text.replace(/\bbot_\S*/gi, '***');
-    }
-};
